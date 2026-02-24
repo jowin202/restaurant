be481856c625c5b8eb707c2b919b67ef9e1a50c5
Johannes Winkler <johannes.w@gmx.at>
2026-02-24 08:05:31 +0100

    fix

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
       { path: 'items/:id/edit', canActivate: [authGuardAdmin], loadComponent: () => import('./components/item-edit/item-edit').then((m) => m.ItemEdit) },
       { path: 'items', redirectTo: 'items/list', pathMatch: 'full' },
       { path: 'users', canActivate: [authGuardSuperAdmin], loadComponent: () => import('./components/user-table/user-table').then((m) => m.UserTable) },
-      { path: 'settings', canActivate: [authGuardSuperAdmin], loadComponent: () => import('./components/settings-window/settings-window').then((m) => m.SettingsWindow) },
+      //{ path: 'settings', canActivate: [authGuardSuperAdmin], loadComponent: () => import('./components/settings-window/settings-window').then((m) => m.SettingsWindow) },
       { path: '', loadComponent: () => import('./components/role-home/role-home').then((m) => m.RoleHome), pathMatch: 'full' },
     ],
   },
